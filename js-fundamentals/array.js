57a39f57c6c5b6f387397f15f51f92d8824f9df5
niharika2k00 <dniharika16@gmail.com>
2026-05-23 02:25:31 +0530

    Refactor: Enhance array.js with additional examples and improve comments for clarity

--- js-fundamentals/array.js
+++ js-fundamentals/array.js
@@ -11,6 +11,8 @@ console.log(arr.toString());
 console.log(arr.join("--"));
 console.log(arr.concat(num));
 console.log(num.reverse()); // update original array
+console.log(arr.indexOf("guava")); // array.indexOf(item, start) | returns index (default start = 0) and -1 if not found
+console.log(arr.includes("apple"));
 
 const arr1 = [
   ["1", "2"],
@@ -19,15 +21,18 @@ const arr1 = [
 // array.flat([depth]) | return new array
 console.log(arr1.flat(1)); // Infinity to flatten the array
 
+// array.flatMap(function(value, index, arr), thisValue) | map + flat in one step
+arr.flatMap((x) => [x, x * 2]);
+
 arr.pop(); // remove an element from the end
-arr.shift(); // remove an element from the beginning
+arr.shift(); // remove an element from the front
 num.unshift(99); // add an element at the front
 
-// array.slice(start, end) [exclusive end] and return new array
+// array.slice(start, end) [exclusive end] | return new array | no change in original array
 let newArr = num.slice(1, 4);
 console.log(newArr);
 
-// array.splice(idx, count, item1, item2,...) | update original array | return new array containing the removed items
+// array.splice(idx, count, item1, item2,...) | update original array | return new array containing the deleted items
 console.log(arr.splice(1, 2));
 
 console.log("array: ", arr);
@@ -36,12 +41,6 @@ console.log("number", num);
 // array.some(function(value, index, arr), this) | return boolean | checks whether at least 1 item of the array satisfies the condition
 let resOfSome = arr.some((val) => val == "apple");
 console.log(resOfSome);
-
-// array.map(function(value, index, arr), thisValue) | return new array
-let doubled = num.map((val, idx) => {
-  return val * 2;
-});
-console.log(doubled);
 
 // array.filter(function(value, index, arr), thisValue) | return new array that passes the condition
 let filteredArray = arr.filter((val) => val > 50);
@@ -59,43 +58,29 @@ const maxItem = num.reduce((accumulator, curr) => {
 }, 0);
 console.log("Max item: ", maxItem);
 
+// array.fill(value, start, end) | return array
+fruits.fill("Kiwi", 2, 4);
+
 const total = num.reduce((accumulator, curr) => accumulator + curr);
 console.log("Sum of the num array: ", total);
 
 // array.every(function(value, index, arr), thisValue) | return boolean if all element pass the test
 let allNegative = num.every((curr, idx) => curr < 0);
-console.log("Is all elements are negative? ", allNegative);
+console.log("Is all elements are negative ? ", allNegative);
 
+// array.forEach(function(value, index, arr), thisValue)
+arr.forEach((val, idx) => {
+  console.log("key: ", idx, " value: ", val);
+});
 
-
+// array.map(function(value, index, arr), thisValue) | return new array
+let doubled = num.map((val, idx) => {
+  return val * 2;
+});
+console.log(doubled);
 
 console.log("array: ", arr);
 console.log("number", num);
-
-// // every ()  :  array.every(function(currentValue, index, arr), thisValue)
-// var num = [2, 45, -100, 96, -32];
-// console.log("Result of every()  =>  ", num.every(allPositive)); //  false
-
-// function allPositive(ele, index, arr) {
-//   console.log("index: ", index);
-//   return ele > 0;
-// }
-
-// //  array.includes(element, start)
-// let text = "hello world";
-// text.includes("world");
-
-// console.table(prep);
-
-// //   fill ()  :   array.fill(value, start, end)
-// /*
-//     -  Overwrite the Original Array a
-//     -  Replace the Element
-// */
-// fruits = ["Banana", "Orange", "Apple", "Mango"];
-// fruits.fill("Kiwi");
-
-// console.log(fruits);
 
 // -----------------------------------------------------------
 //                CALL() , APPLY() , BIND()   METHOD        https://www.w3schools.com/js/js_function_bind.asp
